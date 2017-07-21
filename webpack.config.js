2b880fc3294842cc63a9d25df565f4e1f718c423
dsantacruz <diegomiguel114@gmail.com>
2017-07-21 17:32:45 -0500

    add hot module replacement + variables prod dev

--- webpack.config.js
+++ webpack.config.js
@@ -15,84 +15,107 @@ var webpack = require("webpack");
 //dirname es una variable de entorno que apunta hacia la ruta base, con esto decimos que todos los archivos fuente se encuentra en ese directorio
 
 //dist: todo lo que genere webpack lo va a generar en el dist
-module.exports = {
-    context: path.resolve(__dirname, "src"),
-    entry: {
-        app: './app.js',
-        mobile: './mobile.js',
-        vendor: ['react', 'react-dom', 'rxjs']
-    },
-    output: {
-        filename: './[name]-bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-        rules: [
-            {//extrae a un fichero externo
-                test: /\.css$/,
-                use: extractCSS.extract({
+module.exports = function (env){
+    var isProd = false;
+
+    if(env != null && env.production)
+    {
+        isProd = true;   
+    }
+
+    var configUseCssDev = ["style-loader", "css-loader"];
+    var configUseCssProd = extractCSS.extract({
                     fallback: "style-loader",
                     use: "css-loader"
                 })
-            },
-            {
-                test: /\.scss$/,
-                /*use: [{//genera una etiqueta style en el html
-                    loader: "style-loader" // creates style nodes from JS strings
-                }, {//carga la hoja de estilos dentro del bundle.js
-                    loader: "css-loader" // translates CSS into CommonJS
-                }, {//convierte el lenguaje sass en una hoja de estilos normal css
-                    loader: "sass-loader" // compiles Sass to CSS
-                }]*/
-                use: extractLESS.extract({
-                    fallback: 'style-loader',
-                    //resolve-url-loader may be chained before sass-loader if necessary
-                    use: ['css-loader', 'sass-loader']
-                })
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: "babel-loader",
-                /*options: {
-                    "presets": ["env", "react"]
-                } */
-            }
+
+    var configUseCss = isProd ? configUseCssProd: configUseCssDev;
+
+    var configUseScssDev = ["style-loader", "css-loader", "sass-loader"];
+    var configUseScssProd = extractLESS.extract({
+                        fallback: 'style-loader',
+                        //resolve-url-loader may be chained before sass-loader if necessary
+                        use: ['css-loader', 'sass-loader']
+                    })
+    
+    var configUseScss = isProd ? configUseScssProd: configUseScssDev;
+
+    return {
+
+        context: path.resolve(__dirname, "src"),
+        entry: {
+            app: './app.js',
+            mobile: './mobile.js',
+            vendor: ['react', 'react-dom', 'rxjs']
+        },
+        output: {
+            filename: './[name]-bundle.js',
+            path: path.resolve(__dirname, 'dist')
+        },
+        module: {
+            rules: [
+                {//extrae a un fichero externo
+                    //se comento el uso de extractCSS porque no funciona el plugin hotmodulereplacement
+                    test: /\.css$/,
+                    use: configUseCss
+                },
+                {
+                    test: /\.scss$/,
+                    /*use: [{//genera una etiqueta style en el html
+                        loader: "style-loader" // creates style nodes from JS strings
+                    }, {//carga la hoja de estilos dentro del bundle.js
+                        loader: "css-loader" // translates CSS into CommonJS
+                    }, {//convierte el lenguaje sass en una hoja de estilos normal css
+                        loader: "sass-loader" // compiles Sass to CSS
+                    }]*/
+                    use: configUseScss
+                },
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: "babel-loader",
+                    /*options: {
+                        "presets": ["env", "react"]
+                    } */
+                }
+            ]
+        },
+        devServer: {
+            contentBase: path.join(__dirname, "dist"), //especificar carpeta donde podamos meter contenido estatico como ficheros, imagenes, etc
+            compress: true, //el contenido que sirva el servidor web ira comprimido
+            publicPath: "/assets/", //prefijo en la url
+            //quiet: true, //evitamos que aparezca los mensajes de log en la terminal,
+            stats: "errors-only", //solo muestra los mensajes que sean error
+            port: 9000, //cambiar el puerto por defecto del servidor web
+            hot: true
+        },
+        plugins: [
+            extractCSS,
+            extractLESS,
+            new HtmlWebpackPlugin({
+                title: 'My App',
+                filename: './admin.html',
+                hash: true,
+                chunks: ['commons', 'vendor', 'app'],
+                template: './my-index.ejs'
+            }),
+            new HtmlWebpackPlugin({
+                title: 'My App',
+                filename: './mobile.html',
+                hash: true,
+                chunks: ['commons', 'vendor', 'mobile'],
+                template: './my-index-mobile.ejs'
+            }),
+            new webpack.optimize.CommonsChunkPlugin({
+                name: ['commons', 'vendor']
+            }),
+            new webpack.HotModuleReplacementPlugin() // Enable HMR
+            //if you want to pass in options, you can do so:
+            //new ExtractTextPlugin({
+            //  filename: 'style.css'
+            //})
         ]
-    },
-    devServer: {
-        contentBase: path.join(__dirname, "dist"), //especificar carpeta donde podamos meter contenido estatico como ficheros, imagenes, etc
-        compress: true, //el contenido que sirva el servidor web ira comprimido
-        publicPath: "/assets/", //prefijo en la url
-        //quiet: true, //evitamos que aparezca los mensajes de log en la terminal,
-        stats: "errors-only", //solo muestra los mensajes que sean error
-        port: 9000 //cambiar el puerto por defecto del servidor web
-    },
-    plugins: [
-        extractCSS,
-        extractLESS,
-        new HtmlWebpackPlugin({
-            title: 'My App',
-            filename: './admin.html',
-            hash: true,
-            chunks: ['commons', 'vendor', 'app'],
-            template: './my-index.ejs'
-        }),
-        new HtmlWebpackPlugin({
-            title: 'My App',
-            filename: './mobile.html',
-            hash: true,
-            chunks: ['commons', 'vendor', 'mobile'],
-            template: './my-index-mobile.ejs'
-        }),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: ['commons', 'vendor']
-        })
-        //if you want to pass in options, you can do so:
-        //new ExtractTextPlugin({
-        //  filename: 'style.css'
-        //})
-    ]
+    }
 }
 //el orden de como se aplicaran los loaders, es desde el final hacia el principio, es decir primero se aplica el css-loader luego el style-laoder
 //el css-loader carga el fichero dentro del bundle js pero no lo utiliza.
